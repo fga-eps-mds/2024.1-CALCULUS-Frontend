d0578c191220898549594115e89934608afbbc48
DaviMatheus <matdavimat@gmail.com>
2024-07-24 19:26:50 -0300

    initial_arquiteture

---
+++ src/app/components/Button/Button.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+interface ButtonProps {
+  label: string;
+  backgroundColor?: string;
+  textColor?: string;
+  shadowColor?: string;
+  onClick?: () => void;
+  className?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  backgroundColor = '#FF4122', 
+  textColor = '#FFFAFA',     
+  shadowColor = '#C61A09',
+  onClick,
+  className = '',
+}) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`btn-custom ${className}`} // Adiciona a classe para estilos customizados
+      style={{
+        backgroundColor,
+        color: textColor,
+        boxShadow: `0 4px 6px ${shadowColor}`,
+      }}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
