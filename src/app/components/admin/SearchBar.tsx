28a401d07c52db98d59122200294d99d17b70a65
Nanashii76 <pauloamino76@gmail.com>
2024-07-25 21:48:45 -0300

    feat(#71): Componente SearchBar Criando componente SearchBar para administrador poder buscar por outros usuários
    Co-authored-by: joaobisi <jbisinotti@gmail.com>
    Co-authored-by: Neoprot <kaua.seichi.gomes@gmail.com>

---
+++ src/app/components/admin/SearchBar.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { TextField, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+interface SearchBarProps {
+  value: string;
+  onChange: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ value, onChange }) => {
+  return (
+    <TextField
+  label=""
+  variant="outlined"
+  fullWidth
+  value={value}
+  onChange={(e) => onChange(e.target.value)}
+  InputProps={{
+    startAdornment: (
+      <IconButton>
+        <SearchIcon />
+      </IconButton>
+    ),
+  }}
+  sx={{
+    backgroundColor: 'white', 
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#dcdcdc',
+        borderRadius: '50px',
+      },
+      '&:hover fieldset': {
+        borderColor: '#a0a0a0', 
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#a0a0a0',
+      },
+    },
+  }}
+/>
+  );
+};
+
+export default SearchBar;
