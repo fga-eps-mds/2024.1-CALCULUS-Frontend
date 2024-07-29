4a5c51f10ac3207deb3be24ce99f9d10f6ee961f
Marco Marques de Castro <marcomarquesdecastro@gmail.com>
2024-07-29 00:12:18 -0300

    feat (#69): create responsive Sign Up page

--- src/app/cadastro1/page.tsx
+++ src/app/cadastro1/page.tsx
@@ -1,80 +1,57 @@
 'use client';
 
-import * as React from 'react';
+import robotImage from '@/public/mao_cerebro.png';
 import Image from 'next/image';
-import Link from 'next/link'
-import Head from 'next/head'
-
-import SignUpButton from '../components/Button/SignUpButton';
-import Button from '../components/Button/Button';
-import cadastro1 from './cadastro1.module.css';
-import mao_cerebro from '/src/public/mao_cerebro.png';
+import SocialButton from '../components/Button/SocialButton';
 
 export default function SignUp() {
-  const handleSubmit = (event: { preventDefault: () => void; currentTarget: HTMLFormElement | undefined; }) => {
+  const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
+    const data = new FormData(event.currentTarget as HTMLFormElement);
     console.log({
       email: data.get('email'),
-      password: data.get('password'),
     });
   };
 
-  const handleClick = (source: String) => {
-    alert(`Botão clicado: ${source}`);
-  };
-
   return (
-    <body>
-        <Head>
-        <title>My page title</title>
-        </Head>
-        <div className={cadastro1.outerBox}>
-            <Image
-                src={mao_cerebro}
-                width={368}
-                height={368}
-                alt='A picture'
-            />
-            <div className={cadastro1.container}>
-                <p>Cadastre-se gratuitamente e descubra sua jornada de aprendizado.</p>
-                <div className={cadastro1.buttonsContainer}>
-                    <Button
-                        label="Google"
-                        onClick={() => handleClick('google')}
-                        backgroundColor='#FFFAFA'
-                        textColor='black'
-                        shadowColor='#E0E0E0'
-                    />
-                    <Button
-                        label="Microsoft"
-                        onClick={() => handleClick('microsoft')}
-                        backgroundColor='#FFFAFA'
-                        textColor='black'
-                        shadowColor='#E0E0E0'
-                    />
-                    <Button
-                        label="Apple"
-                        onClick={() => handleClick('apple')}
-                        backgroundColor='#FFFAFA'
-                        textColor='black'
-                        shadowColor='#E0E0E0'
-                    />
-
-                </div>
-                
-                <form className={cadastro1.form} onSubmit={() => alert('Submitting!')}>
-                    <input type='email' placeholder='Email' required/>
-                    <br/>
-                    <SignUpButton
-                        label="Sign Up"
-                        className={cadastro1.sign}
-                    />
-                </form>
-                
-                <Link className={cadastro1.link} href='' title='Log In'>Já possui cadastro?Log in</Link>
-            </div>
+    <div className='min-h-screen flex flex-col justify-center items-center bg-white'>
+      <div className='flex flex-col md:flex-row justify-center items-center w-full max-w-5xl '>
+        <div className='md:w-1/2 flex justify-center mb-8 md:mb-0'>
+          <Image src={robotImage} alt='Robot Image' width={400} height={400} />
         </div>
-    </body>
+        <div className='md:w-1/2 bg-white p-8 rounded-lg shadow-lg'>
+          <h1 className='text-2xl font-bold text-center mb-4 text-gray-800'>
+            Cadastre-se gratuitamente e descubra sua jornada de aprendizado.
+          </h1>
+          <div className='flex space-x-4 justify-center mb-4'>
+            <SocialButton provider='google' />
+            <SocialButton provider='microsoft' />
+          </div>
+          <form onSubmit={handleSubmit} className='space-y-6'>
+            <div>
+              <input
+                type='email'
+                name='email'
+                id='email'
+                required
+                className='w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-gray-800'
+                placeholder='Email'
+              />
+            </div>
+            <button
+              type='submit'
+              className='w-full bg-black text-white px-4 py-2 rounded-full font-semibold hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800'
+            >
+              Sign Up
+            </button>
+          </form>
+          <div className='mt-4 text-center'>
+            <a href='#' className='text-gray-800 hover:underline'>
+              Já possui cadastro? Log in
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
