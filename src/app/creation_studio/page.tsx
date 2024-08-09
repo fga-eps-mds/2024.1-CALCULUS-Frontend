5aa8b2a54f90dbe20e6ee1ec50f3db1dc4716c66
NATAN <natan.almeida03@gmail.com>
2024-08-09 10:52:53 -0300

    feat#74: Conversor automatico de markdown.
    adicionando o conversor de markdown automatico para que o usuario consiga adionar conteudo

--- src/app/creation_studio/page.tsx
+++ src/app/creation_studio/page.tsx
@@ -1,25 +1,54 @@
 'use client';
 import '../../styles/creation.css';
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
+import React from 'react';
+import Box from '@mui/material/Box';
 
 export default function CreationStudio() {
-  
-    return (
-      <div className="principal">
-      <div className="container">
-      <div className="editor-section">
-          <div id="botoes">
-              <button><strong>N</strong></button>
-              <button><strong><em>S</em></strong></button>
-              <button><strong><s>S</s></strong></button>
-          </div>
-          <div className="editor">
-              <textarea id="editor" placeholder="Insira seu texto aqui..."></textarea>
-          </div>
-      </div>
-      <div className="preview-section">
-          <div id="preview" className="preview-content"></div>
-      </div>
-  </div>
-  </div>
-    );
-  }
+  const [input, setInput] = React.useState('');
+
+  return (
+    <Box className="h-screen w-screen overflow-hidden">
+      <Box
+        id="botoes"
+        className="bg-[#f8f3f3] border-2 border-solid border-[#D9D9D9]"
+      >
+        <button>
+          <strong>N</strong>
+        </button>
+        <button>
+          <strong>
+            <em>S</em>
+          </strong>
+        </button>
+        <button>
+          <strong>
+            <s>S</s>
+          </strong>
+        </button>
+      </Box>
+      <Box className="grid grid-cols-2 h-full">
+        <textarea
+          autoFocus
+          className="bg-[#fffafa] px-5 py-3 leading-2 border"
+          id="editor"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+        <Box className="bg-[#f8f3f3] px-5 py-3 border-2 border-solid border-[#D9D9D9]">
+          <Markdown
+            className="leading-2"
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeRaw]}
+          >
+            {input}
+          </Markdown>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
