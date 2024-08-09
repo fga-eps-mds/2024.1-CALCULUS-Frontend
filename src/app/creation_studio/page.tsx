fd6da71cb22127b9d7e1659a525a49f66590257d
Marco Marques <marcomarquesdecastro@gmail.com>
2024-08-09 01:04:46 -0300

    Adicionando estrutura de botões para o editor de texto. Co-authored-by: PedroCruz <pedrofcrux@gmail.com>

--- src/app/creation_studio/page.tsx
+++ src/app/creation_studio/page.tsx
@@ -4,23 +4,22 @@ import '../../styles/creation.css';
 export default function CreationStudio() {
   
     return (
-    <div className='principal'>
-    {/*botões */}
-
-      <div>
-          <button><strong>N</strong></button>
-          <button><strong><em>S</em></strong></button>
-          <button><strong><s>S</s></strong></button>
+      <div className="principal">
+      <div className="container">
+      <div className="editor-section">
+          <div id="botoes">
+              <button><strong>N</strong></button>
+              <button><strong><em>S</em></strong></button>
+              <button><strong><s>S</s></strong></button>
+          </div>
+          <div className="editor">
+              <textarea id="editor" placeholder="Insira seu texto aqui..."></textarea>
+          </div>
       </div>
-    
-    <div className="container">
-    <div className="editor">
-        <textarea id="editor" placeholder="Insira seu texto aqui..."></textarea>
-    </div>
-    <div className="preview">
-        <div id="preview" className="preview-content"></div>
-    </div>
-    </div>
-    </div>
+      <div className="preview-section">
+          <div id="preview" className="preview-content"></div>
+      </div>
+  </div>
+  </div>
     );
   }
