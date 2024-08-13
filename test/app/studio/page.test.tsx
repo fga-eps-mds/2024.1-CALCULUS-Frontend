4e98ed017524bb6fff926cb67b79647038d61c78
Pedro Cruz <pedrofcrux@gmail.com>
2024-08-13 11:03:38 -0300

    teste da função de imagem

--- test/app/studio/page.test.tsx
+++ test/app/studio/page.test.tsx
@@ -112,6 +112,27 @@ italic
 $$
 `);
     })
+
+    it('get the URL of the image and render image', () =>{
+        global.prompt = jest.fn().mockReturnValue(
+'https://i0.wp.com/thecaninebuddy.com/wp-content/uploads/2021/08/crying-cat-meme.jpg?resize=1024%2C576&ssl=1'
+        );
+
+        render(<MarkdownEditor />);
+        
+
+        const image = screen.getByRole('button', {name:"Imagem"});
+        
+        const textareaRef = screen.getByRole('textbox');
+
+        fireEvent.click(image);
+
+
+        expect(textareaRef).toHaveValue(
+"![Descrição da imagem](https://i0.wp.com/thecaninebuddy.com/wp-content/uploads/2021/08/crying-cat-meme.jpg?resize=1024%2C576&ssl=1)"
+        )
+
+    })
 })
 
 
