b555ff3c51224be78572388424ddec2da9d82e3a
joaobisi <jbisinotti@gmail.com>
2024-08-16 15:43:56 -0300

    feat(#74): Reposicionando botao Salvar

--- src/app/components/studio/MarkdownToolbar.tsx
+++ src/app/components/studio/MarkdownToolbar.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { IconButton, Tooltip, Divider, Box } from '@mui/material';
-import { 
-    FormatBold as BoldIcon, 
-    FormatItalic as ItalicIcon, 
-    Title as TitleIcon, 
-    Image as ImageIcon, 
-    Functions as FunctionsIcon, 
-    Save as SaveIcon, 
-    FormatListBulleted as FormatListBulletedIcon, 
-    Folder as FolderIcon 
+import { IconButton, Tooltip, Divider, Box, Toolbar, AppBar } from '@mui/material';
+import {
+    FormatBold as BoldIcon,
+    FormatItalic as ItalicIcon,
+    Title as TitleIcon,
+    Image as ImageIcon,
+    Functions as FunctionsIcon,
+    Save as SaveIcon,
+    FormatListBulleted as FormatListBulletedIcon,
+    Folder as FolderIcon
 } from '@mui/icons-material';
 
 interface ToolbarProps {
@@ -19,52 +19,52 @@ interface ToolbarProps {
 }
 
 const MarkdownToolbar: React.FC<ToolbarProps> = ({ toggleSidebar, insertTextAtSelection, insertImage, handleSave }) => (
-    <Box className="flex items-center gap-2">
-        <Tooltip title="Arquivos">
-        <IconButton className="text-[#6667AB]" onClick={toggleSidebar}>
-            <FolderIcon sx={{ fontSize: 32 }} />
-        </IconButton>
-        </Tooltip>
-
-        <Divider orientation="vertical" flexItem className="bg-[#D9D9D9]" />
-
-        <Tooltip title="Imagem">
-        <IconButton onClick={insertImage}>
-            <ImageIcon />
-        </IconButton>
-        </Tooltip>
-        <Tooltip title="Negrito">
-        <IconButton onClick={() => insertTextAtSelection('**', '**', 'texto em negrito')}>
-            <BoldIcon />
-        </IconButton>
-        </Tooltip>
-        <Tooltip title="Itálico">
-        <IconButton onClick={() => insertTextAtSelection('*', '*', 'texto em itálico')}>
-            <ItalicIcon />
-        </IconButton>
-        </Tooltip>
-        <Tooltip title="Título">
-        <IconButton onClick={() => insertTextAtSelection('# ', '\n', 'Título')}>
-            <TitleIcon />
-        </IconButton>
-        </Tooltip>
-        <Tooltip title="Lista">
-        <IconButton onClick={() => insertTextAtSelection('- ', '\n', 'Item da lista')}>
-            <FormatListBulletedIcon />
-        </IconButton>
-        </Tooltip>
-        <Tooltip title="LaTeX">
-        <IconButton onClick={() => insertTextAtSelection('$$\n', '\n$$\n', 'E = mc^2')}>
-            <FunctionsIcon />
-        </IconButton>
-        </Tooltip>
-
-        <Tooltip title="Salvar">
-        <IconButton className="text-[#515287]" onClick={handleSave}>
-            <SaveIcon sx={{ fontSize: 35 }} />
-        </IconButton>
-        </Tooltip>
-    </Box>
+    <><Toolbar className="flex justify-between items-center">
+        <Box className="flex items-center gap-2">
+            <Tooltip title="Arquivos">
+                <IconButton className="text-[#6667AB]" onClick={toggleSidebar}>
+                    <FolderIcon sx={{ fontSize: 32 }} />
+                </IconButton>
+            </Tooltip>
+            <Divider orientation="vertical" flexItem className="bg-[#D9D9D9]" />
+            <Tooltip title="Imagem">
+                <IconButton onClick={insertImage}>
+                    <ImageIcon />
+                </IconButton>
+            </Tooltip>
+            <Tooltip title="Negrito">
+                <IconButton onClick={() => insertTextAtSelection('**', '**', 'texto em negrito')}>
+                    <BoldIcon />
+                </IconButton>
+            </Tooltip>
+            <Tooltip title="Itálico">
+                <IconButton onClick={() => insertTextAtSelection('*', '*', 'texto em itálico')}>
+                    <ItalicIcon />
+                </IconButton>
+            </Tooltip>
+            <Tooltip title="Título">
+                <IconButton onClick={() => insertTextAtSelection('# ', '\n', 'Título')}>
+                    <TitleIcon />
+                </IconButton>
+            </Tooltip>
+            <Tooltip title="Lista">
+                <IconButton onClick={() => insertTextAtSelection('- ', '\n', 'Item da lista')}>
+                    <FormatListBulletedIcon />
+                </IconButton>
+            </Tooltip>
+            <Tooltip title="LaTeX">
+                <IconButton onClick={() => insertTextAtSelection('$$\n', '\n$$\n', 'E = mc^2')}>
+                    <FunctionsIcon />
+                </IconButton>
+            </Tooltip>
+        </Box>
+    </Toolbar><Box>
+            <Tooltip title="Salvar">
+                <IconButton className="text-[#515287]" onClick={handleSave}>
+                    <SaveIcon sx={{ fontSize: 35 }} />
+                </IconButton>
+            </Tooltip>
+        </Box></>
 );
 
 export default MarkdownToolbar;
