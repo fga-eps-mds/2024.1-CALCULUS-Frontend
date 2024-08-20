c4e9b4622026ac9383c8152ae88f4d30372fed25
Pedro Cruz <pedrofcrux@gmail.com>
2024-08-20 16:49:42 -0300

    teste do signout button

--- test/app/homePage/home.test.tsx
+++ test/app/homePage/home.test.tsx
@@ -1,11 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'; 
 import HomePage from '@/app/home/page';
-import { useSession } from 'next-auth/react';
+import { useSession, signOut} from 'next-auth/react';
 
 // Mock do useSession
 jest.mock('next-auth/react', () => ({
   useSession: jest.fn(),
+  signOut: jest.fn()
 }));
 
 describe('HomePage', () => {
@@ -30,6 +31,28 @@ describe('HomePage', () => {
 
     // Verificar se o botão de sair está presente
     expect(screen.getByRole('button', { name: /Sair/i })).toBeInTheDocument();
+    
+
   });
+
+  test('logout', () =>{
+    (useSession as jest.Mock).mockReturnValue({
+      data: {
+        user: {
+          name: 'John Doe',
+          email: 'john.doe@example.com',
+        },
+      },
+      status: 'authenticated',
+  });
+
+  render(<HomePage />);
+
+  const button = screen.getByText("Sair");
+        button.click()
+
+        expect(localStorage.getItem('token')).not
+
+  })
 
 });
