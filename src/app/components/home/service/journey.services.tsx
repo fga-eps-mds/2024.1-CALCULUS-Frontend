e575fedf459c6ffbd14128359fb53dcab972b5bf
Neoprot <kaua.seichi.gomes@gmail.com>
2024-08-23 10:04:00 -0300

    feat(#79):Ajustando rotas Ajustando rotas da services que antes estavam erradas

--- src/app/components/home/service/journey.services.tsx
+++ src/app/components/home/service/journey.services.tsx
@@ -1,32 +1,46 @@
 import axios from "axios";
-import { useSession } from 'next-auth/react';
 import { toast } from "sonner";
 
 const JourneyService = () => {
-    const { data: session } = useSession();
-    const fetchUserJourneys = async () => {
-        if (!session){
-            toast.error('Você precisa estar logado para salvar o conteúdo.');
-            return;
-        };
+    const fetchUserJourneys = async (session: any) => {
+        if (!session) {
+            toast.error('Você precisa estar logado para ver suas jornadas.');
+            return [];
+        }
         try {
-            const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL_USER}/${session.user.id}/subscribedJourneys`);
+            const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL_USER}users/${session.user.id}/subscribedJourneys`);
             return response.data;
         } catch (error) {
             console.error('Erro ao buscar jornadas:', error);
             return [];
         }
-    }
+    };
 
     const fetchJourneys = async () => {
         try {
-            const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL_STUDIO}/journeys/`);
+            const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL_STUDIO}journeys/`);
             return response.data;
         } catch (error) {
             console.error('Erro ao buscar jornada:', error);
             return null;
         }
-    }
-}
+    };
+
+    const fetchJourneyById = async (id: string) => {
+        try {
+            const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL_STUDIO}journeys/${id}`);
+            return response.data;
+        } catch (error) {
+            console.error(`Erro ao buscar jornada com ID ${id}:`, error);
+            return null;
+        }
+    };
+
+    return {
+        fetchUserJourneys,
+        fetchJourneys,
+        fetchJourneyById,
+    };
+};
 
 export default JourneyService;
