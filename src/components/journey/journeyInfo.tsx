0588f110e638112916863b15658ec012cd647934
unknown <pauloamino76@gmail.com>
2024-08-23 23:26:49 -0300

    feat(#78)/progresso-da-trilha: Criando página da Jornada
    Criando página de ingressão e visualização de progresso
    
    Co-authored-by: Joa0V  <joao.vitortnt5@gmail.com>
    Co-authored-by: joaobisi <jbisinotti@gmail.com>

--- src/components/journey/journeyInfo.tsx
+++ src/components/journey/journeyInfo.tsx
@@ -1,88 +1,127 @@
-'use client'
+'use client';
 
 import React from 'react';
 import { Box, Button, Typography } from '@mui/material';
 import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark';
 import PercentIcon from '@mui/icons-material/Percent';
 import { PeopleAlt } from '@mui/icons-material';
+import MyButton from '@/components/ui/buttons/myButton.component';
 
 interface JourneyInfoProps {
-    title: string;
-    description: string;
-    trailCount: number;
-    completionPercentage: number;
-    completedStudents: number;
+  title: string;
+  description: string;
+  trailCount: number;
+  completionPercentage: number;
+  completedStudents: number;
 }
 const JourneyInfo: React.FC<JourneyInfoProps> = ({
-    title,
-    description,
-    trailCount,
-    completionPercentage,
-    completedStudents
+  title,
+  description,
+  trailCount,
+  completionPercentage,
+  completedStudents,
 }) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '25px',
+        borderRadius: '20px',
+        backgroundColor: '#FFF',
+        boxShadow: '0 4px 8px rgba(0,0,0,0.5)',
+        maxWidth: '650px',
+      }}
+    ml={20}>
+      <Typography
+        variant="h3"
+        fontWeight={'bold'}
+        sx={{ fontFamily: 'Poppins, sans-serif' }}
+      >
+        {title}
+      </Typography>
 
-    return(
+      <Box mb={9} mt={6}>
+        <p className="text-2xl">{description}</p>
+      </Box>
 
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            padding: '15px',
-            borderRadius: '20px',
-            backgroundColor: '#FFAFA',
-            boxShadow: '0 4px 8px rgba(0,0,0,0.5)',
-            maxWidth: '800px'
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '8px',
+          alignItems: 'center',
         }}
-        mt={10}
-        ml={40}
+        mb={1}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
         >
-        <Typography  variant='h3' fontWeight={'bold'} sx={{fontFamily: 'Poppins, sans-serif'}} >{title}</Typography>
-        
-        <Box mb={9} mt={6}>
-            <p className='text-lg'>{description}</p>
-        </Box>
-        
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '8px',
-        }}
-        mb={1}>
-            <Box sx={{
-                display: 'flex',
-                alignItems: 'center'
-            }}><CollectionsBookmarkIcon /></Box>
-        
-            <Typography variant='body2'>Número de trilhas: {trailCount}</Typography>
-        </Box>
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '8px'
-        }}
-        mb={1}>
-            <Box sx={{
-                display: 'flex',
-                alignItems: 'center'
-            }}><PercentIcon /></Box>
-        
-        <Typography variant='body2'>Porcentagem de conteúdo completada: {completionPercentage}</Typography>
-        </Box>
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '8px'
-        }}
-        mb={1}>
-            <Box sx={{
-                display: 'flex',
-                alignItems: 'center'
-            }}><PeopleAlt /></Box>
-        
-        <Typography variant='body2'>Alunos que completaram a jornada: {completedStudents}</Typography>
+          <CollectionsBookmarkIcon />
         </Box>
 
+        <Typography variant="body2">Número de trilhas: {trailCount}</Typography>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '8px',
+          alignItems: 'center',
+        }}
+        mb={1}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <PercentIcon />
         </Box>
-    );
-}
+
+        <Typography variant="body2">
+          Porcentagem de conteúdo completada: {completionPercentage}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '8px',
+          alignItems: 'center',
+        }}
+        mb={1}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <PeopleAlt />
+        </Box>
+
+        <Typography variant="body2">
+          Alunos que completaram a jornada: {completedStudents}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+        }}
+        ml={55}
+      >
+        <MyButton width="150px" height="50px" color="red" bold>
+          Ingressar
+        </MyButton>
+      </Box>
+    </Box>
+  );
+};
 
 export default JourneyInfo;
